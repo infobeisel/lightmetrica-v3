190830e86331f79dd918d2eddb4a7abddf69694b
PhilippAufmDellLaptop <infobeisel@gmail.com>
2020-10-14 18:22:32 +0200

    move functions to be accessible from other classes.

--- plugin/arepoloader/arepoloader.cpp
+++ plugin/arepoloader/arepoloader.cpp
@@ -481,23 +481,9 @@ namespace ArepoLoaderInternals {
 
     };
 
-    inline double det3x3(lm::Vec3 b,lm::Vec3 c,lm::Vec3 d) {
-        //glm::determinant(lm::Mat3(b0,b1,b2));
-        return b[0]*c[1]*d[2] + c[0]*d[1]*b[2] + d[0]*b[1]*c[2] - d[0]*c[1]*b[2] - c[0]*b[1]*d[2] - b[0]*d[1]*c[2];
-    }
     
-    inline void connectP(glm::tmat4x3<lm::Float> const  & verts, lm::Vec3 const & p, glm::tmat4x3<lm::Float> & pToVerts) {
+    
 
-        for(int i = 0; i < 4; i++)
-            pToVerts[i] = verts[i] - p;
-    }
-
-    inline void computeDeterminants(glm::tmat4x3<lm::Float> & pToVerts, lm::Vec4 & results) {
-        results[0] = det3x3(pToVerts[1],pToVerts[2],pToVerts[3]);
-        results[1] = det3x3(pToVerts[0],pToVerts[2],pToVerts[3]);
-        results[2] = det3x3(pToVerts[0],pToVerts[1],pToVerts[3]);
-        results[3] = det3x3(pToVerts[0],pToVerts[1],pToVerts[2]); 
-    }
 
     //this only works iff the tetrahedron is specified with a specific vertex order, namely, see mesh_arepo.cpp !!!
     inline bool insideOld(lm::Vec4 & determinants) {
@@ -506,10 +492,6 @@ namespace ArepoLoaderInternals {
         return ret0 || ret1;
     }
 
-    //this test works irrespective of vertex order of the tetrahedron
-    inline bool inside(lm::Vec4 & determinants, lm::Float tetraDeterminant) {
-        return abs(abs(tetraDeterminant) - (abs(determinants[0])+abs(determinants[1])+abs(determinants[2])+abs(determinants[3]))) < INSIDE_TOLERANCE;       
-    }
 
 
     inline bool insideCachedTetra(lm::Vec3 p, CachedSample & c) {
